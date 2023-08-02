8cfc686a6806d1ca699228cf7dfbdc2884666f7c
Monique Hayes <moniqueehayes5@gmail.com>
2023-08-02 08:58:36 -0500

    arraySearch2D completed

--- src/arraySearch2D/arraySearch2D.js
+++ src/arraySearch2D/arraySearch2D.js
@@ -1,15 +1,15 @@
 //function that takes in a two-dimensional (2D) array and searches each 2D element for the character `X`
 
 const arraySearch2D = (array) => {
-  let number = 0;
-  array.map((y) =>
-    y.map((x) => {
-      if (x === "X") {
-        number += 1;
-      }
-    })
-  );
-  return number;
+  let count = 0;
+ for (const item of array) {
+  for (let i=0; i<item.length; i++) {
+    if (item[i]=== 'X') {
+      count = count + 1;
+    }
+  }
+ }
+ return count;
 };
 
 module.exports = { arraySearch2D };
